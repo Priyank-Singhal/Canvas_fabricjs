1806b3507460cdecf2c34153f8c0f4fc07e7439e
Priyank Singhal <dpspri12060@gmail.com>
2022-05-31 00:41:18 +0530

    added buttons to add shapes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,17 @@ import { fabric } from "fabric";
 
 const Home = () => {
 
-    const [canvas, setCanvas] = useState('');
+    let canvas = new fabric.Canvas();
 
-    const initCanvas = () => (
-        new fabric.Canvas('c', {
+    useEffect(() => {
+        canvas = new fabric.Canvas('c', {
             height: 800,
             width: 1000,
             backgroundColor: 'pink'
         })
-    );
+    })
 
     const makeRect = () => {
-        const canvas = new fabric.Canvas('c');
         const rect = new fabric.Rect({
             left: 100,
             top: 100,
@@ -25,21 +24,16 @@ const Home = () => {
         canvas.add(rect);
     }
 
-    const makeCircle =() => {
-        const canvas = new fabric.Canvas('c');
+    const makeCircle = () => {
         const circle = new fabric.Circle({
             left: 150,
             top: 150,
             fill: 'orange',
             radius: 50
         })
-        canvas.add(circle)
+        canvas.add(circle);
     }
-
-    useEffect(() => {
-        setCanvas(initCanvas());
-    }, []);
-
+        
     return (
         <div>
             <h1>This is the Heading 1</h1>
@@ -48,7 +42,6 @@ const Home = () => {
             <button>Ellipse</button>
             <button onClick={makeRect}>Rectangle</button>
             <button>Triangle</button>
-            {/* <button></button> */}
             <canvas id="c" />
         </div>
     )
