829638a745185cf0c6c25b800bd1d9374ef91d3b
Priyank Singhal <dpspri12060@gmail.com>
2022-05-31 20:15:54 +0530

    added mouse events

--- src/pages/Home3.js
+++ src/pages/Home3.js
@@ -1,35 +1,65 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { fabric } from 'fabric';
 
-const makeRect = () => {
-    var canvas = new fabric.Canvas('c', {
-        width: 800,
-        height: 800,
-        backgroundColor: 'grey'
-    });
-
-    // create a rectangle object
-    var rect = new fabric.Rect({
-        left: 100,
-        top: 100,
-        fill: 'red',
-        width: 20,
-        height: 20
-    });
-
-    // "add" rectangle onto canvas
-    canvas.add(rect);
-}
-
-
 const Home3 = () => {
-    useEffect(()=> {
-        makeRect();
-    },[])
+    const [x, setX] = useState(50)
+    const [y, setY] = useState(50)
+    const [w, setW] = useState(90)
+    const [h, setH] = useState(90)
+
+    const makeRect = () => {
+        const canvas = new fabric.Canvas('c', {
+            width: 1900,
+            height: 850,
+            // backgroundColor: 'grey'
+        });
+
+        const rect = new fabric.Rect({
+            left: 50,
+            top: 50,
+            fill: 'red',
+            width: 90,
+            height: 90
+        });
+
+        canvas.add(rect);
+        canvas.on('object:modified', () => {
+            console.log('Modified')
+            const left = rect.get('left')
+            const top = rect.get('top')
+            const width = rect.get('width')
+            const height = rect.get('height')
+            console.log(rect.get('height'))
+            setX(left)
+            setY(top)
+            setW(width)
+            setH(height)
+        })
+        canvas.on('event:dragover', ()=> {
+            console.log(rect.get('height'))
+
+        })
+        rect.set({
+            left: x, top: y,
+            width: w,
+            height: h
+        })
+        canvas.on('mouse:dblclick', () => {
+            console.log('x-axis position: ', rect.get('left'))
+            console.log('Double Click')
+        })
+    }
+
+
+    useEffect(() => {
+        makeRect()
+    })
+
     return (
         <div>
             {/* <button onClick={makeRect}>make rect</button> */}
-            <h2>This is </h2>
+            <h2>This is the </h2>
+            <h3>X coordinate: {x}</h3>
             <canvas id='c'></canvas>
         </div>
     )
