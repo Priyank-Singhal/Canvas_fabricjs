047a231c90d4b7554509f6d7ab3e13dbf3ef7b39
Priyank Singhal <dpspri12060@gmail.com>
2022-05-31 22:55:32 +0530

    app version1.0

--- src/pages/Home3.js
+++ src/pages/Home3.js
@@ -4,19 +4,18 @@ import { fabric } from 'fabric';
 const Home3 = () => {
     const [x, setX] = useState(50)
     const [y, setY] = useState(50)
-    const [w, setW] = useState(90)
-    const [h, setH] = useState(90)
+    const [clicked, setClicked] = useState(false)
 
     const makeRect = () => {
         const canvas = new fabric.Canvas('c', {
             width: 1900,
             height: 850,
-            // backgroundColor: 'grey'
+            backgroundColor: 'grey'
         });
 
         const rect = new fabric.Rect({
-            left: 50,
-            top: 50,
+            left: x,
+            top: y,
             fill: 'red',
             width: 90,
             height: 90
@@ -24,30 +23,23 @@ const Home3 = () => {
 
         canvas.add(rect);
         canvas.on('object:modified', () => {
-            console.log('Modified')
             const left = rect.get('left')
             const top = rect.get('top')
-            const width = rect.get('width')
-            const height = rect.get('height')
-            console.log(rect.get('height'))
-            setX(left)
+            setX(left)  
             setY(top)
-            setW(width)
-            setH(height)
         })
-        canvas.on('event:dragover', ()=> {
-            console.log(rect.get('height'))
-
+        canvas.on('selection:created', ()=>{
+            setClicked(true)
         })
+        canvas.on('selection:cleared', ()=>{
+            setClicked(false)
+        })
+        
         rect.set({
-            left: x, top: y,
-            width: w,
-            height: h
+            left: x,
+            top: y,
         })
-        canvas.on('mouse:dblclick', () => {
-            console.log('x-axis position: ', rect.get('left'))
-            console.log('Double Click')
-        })
+        canvas.renderAll();
     }
 
 
@@ -59,7 +51,9 @@ const Home3 = () => {
         <div>
             {/* <button onClick={makeRect}>make rect</button> */}
             <h2>This is the </h2>
+            <h3>Shape Clicked: {`${clicked}`}</h3>
             <h3>X coordinate: {x}</h3>
+            <h3>Y coordinate: {y}</h3>
             <canvas id='c'></canvas>
         </div>
     )
